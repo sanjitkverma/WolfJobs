b5138cd4e02102806dc3661b239ecb644c480697
vhvuong <vhvuong@ncsu.edu>
2024-10-22 22:40:04 -0400

    minor change to email controller

--- backend/controllers/email_controller.js
+++ backend/controllers/email_controller.js
@@ -1,5 +1,7 @@
 const email = require('../models/email');
 const Application = require('../models/application');
+const User = require('../models/user');
+
 
 const updateApplicationStatus = async (req, res) => {
     const { applicationId, status } = req.body;
@@ -18,8 +20,12 @@ const updateApplicationStatus = async (req, res) => {
       const subject = `Your application has been reviewed`;
       const text = `Dear ${applicant.name},\n\nYour application for the position has been ${status}.\n\nBest regards,\nHiring Team`;
   
-      email.sendEmail(application.applicantid, subject, text);
-  
+      try {
+        await email.sendEmail(applicant._id, subject, text);
+      } catch (emailError) {
+        console.error('Error sending email:', emailError);
+        return res.status(500).json({ message: 'Application status updated, but failed to send email notification' });
+      }  
       res.status(200).json({ message: 'Application status updated and notification sent' });
     } catch (error) {
       res.status(400).json({ message: error.message });
